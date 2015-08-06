93030cf9e1f9b2ca5eb304f703db16fe1f3226ab
貴博 <drmashu@gmail.com>
2015-08-07 01:07:02 +0900

    commit

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # dikon
+**dikon** is "DI CONtainer written by Kotlin".  
+**dikon** is Japanese "Daikon" radish.
+
+Kotlinの勉強を兼ねて、KotlinでDI コンテナを作っています。
+
+最終的にはM(V)Cフレームワークとして、適当なViewと適当なPersistence FWを組み合わせたフルスタックなフレームワークになれば良いなぁ。
--- src/com/github/drmashu/dikon/Dikon.kt
+++ src/com/github/drmashu/dikon/Dikon.kt
@@ -3,6 +3,19 @@ package com.github.drmashu.dikon
 /**
  * Created by NAGASAWA Takahiro on 2015/08/06.
  */
-public class Dikon {
+public class Dikon(val components:Map<String, ComponetFactory>) {
 
+    fun get(name:String) : Object? {
+        var factory = components[name]
+        return if (factory == null) {
+            null
+        } else {
+            factory.create()
+        }
+    }
+
+}
+
+interface ComponetFactory {
+    fun create() : Object?
 }
